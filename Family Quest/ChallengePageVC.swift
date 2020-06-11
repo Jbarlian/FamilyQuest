8878a519c4960f92a8afcb661b996cd033c95218
James Barlian <Jamesbarlian@gmail.com>
2020-06-12 00:00:00 +0700

    Challenges Added

--- Family Quest/ChallengePageVC.swift
+++ Family Quest/ChallengePageVC.swift
@@ -10,6 +10,41 @@ import UIKit
 
 class ChallengePageVC: UIViewController {
 
+    var challenge:[String] = [
+         "Tell and embarrassing story",
+         "Tell your greatest acomplishments",
+         "Tell your biggest fear",
+         "Do a silly dance for 1 minute",
+         "Describe your favorite food and have the others guess it",
+         "Describe your favorite animal and have the others guess it",
+         "Describe your favorite movie and have the others guess it",
+         "What are you most grateful for today?",
+         "Attempt to do cart wheel",
+         "Act out your favorite hobby and have the others guess it",
+         "Act your your least favorite activity and have the others guess",
+         "Describe your favorite drink and have the other guess it",
+         "Describe your favorite fruit and have the others guess it",
+         "Sing your favorite song and have the others guess the artist",
+         "Try to touch your toes without bending your knees",
+         "Describe your favorite vacation and have the others guess it",
+         "Who do you look up to the most, and what qualities of that person do you love?",
+         "What's your favorite memory?",
+         "What is your most valued posession and why?",
+         "Where do you want to travel the most and why?",
+         "If you could only eat one thing for the rest of your life what would it be?",
+         "If you had three wishes what would you wish for?",
+         "What would your superpower be if you could have one and why?",
+         "What foreign language do you want to master and why?",
+         "Describe yourself with only using three words",
+         "What is your most hated chore and why?",
+         "What's your favorite holiday and why?",
+         "If you could time travel, where would you go?"
+        
+        
+        ]
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
